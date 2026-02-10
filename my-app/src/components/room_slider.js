1341f385c8d409840edfc2c5625b672fbb77e797
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-02-10 11:48:09 +0530

    implement the swiper in the room_slider.js

---
+++ my-app/src/components/room_slider.js
@@ -0,0 +1,42 @@
+// Import of Swiper and SwiperSlide
+import {Swiper,SwiperSlide} from 'swiper/react';
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+// Import of the room_slider css file
+import '../components/room_slider.css';
+
+function room_slider({rooms}){
+    return(
+        <div class="mini-slider" style={{width:"100%", height:"100%"}}>
+
+            <Swiper
+                modules={[Navigation,Pagination,Scrollbar,A11y]}
+                slidesPerView={1}
+                navigation
+                pagination={{clickable:true}}
+                style={{width:'100%', height:'100%'}}
+            >
+
+                {/* Here from the rooms is array of room object, so i loop through the rooms and get single room */}
+
+                {rooms.map((room)=>(
+                    <SwiperSlide key={room.image}
+                        style={{
+                            display:'flex',
+                            alignItems:'center',
+                            justifyContent:'center',
+                            backgroundColor:'#1E293B'
+                        }}
+                    >
+                        <img src={room.image} alt={room.title} style={{width:'100%', height:'100%', objectFit:'contain', display:'block'}}/>
+                    </SwiperSlide>
+                ))}
+
+            </Swiper>
+        </div>
+    )
+}
+
+export default room_slider;
