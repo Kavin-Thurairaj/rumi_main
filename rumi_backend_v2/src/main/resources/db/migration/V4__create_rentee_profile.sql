7361f9097cfad831597a8ae21908c637489ed6ba
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-02-20 18:59:17 +0530

    update and fix the date of birth in the migration db files

--- rumi_backend_v2/src/main/resources/db/migration/V4__create_rentee_profile.sql
+++ rumi_backend_v2/src/main/resources/db/migration/V4__create_rentee_profile.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS rentee_profiles (
     user_id VARCHAR(128) PRIMARY KEY,
     preferred_location VARCHAR(255),
     gender ENUM('male','female','other'),
-    age INT,
+    date_of_birth DATE,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
