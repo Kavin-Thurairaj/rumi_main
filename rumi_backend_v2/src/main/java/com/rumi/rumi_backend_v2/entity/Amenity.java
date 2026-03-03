7b4b94da0aefe3e517b713114061766e559bfa5c
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-03 23:01:33 +0530

    remove the args constructor for the amenity class

--- rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/entity/Amenity.java
+++ rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/entity/Amenity.java
@@ -1,15 +1,11 @@
 package com.rumi.rumi_backend_v2.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name="amenity")
-@NoArgsConstructor   // Here a default constructor will be created for the RoomDetail class.
-@AllArgsConstructor  // Here a parameterised constructor will be created for the RoomDetail class.
+@Builder
 public class Amenity {
 
     @Id
---
+++ rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/entity/RoomPrice.java
@@ -0,0 +1,38 @@
+package com.rumi.rumi_backend_v2.entity;
+
+import com.rumi.rumi_backend_v2.enums.BillingCycle;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="room_price")
+public class RoomPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="price_id")
+    @Getter
+    private Long priceId;
+
+    @OneToOne
+    @JoinColumn(name="room_id",nullable=false)
+    @Getter
+    private RoomDetail room;
+
+    @Column(name="billing_cycle", nullable=false)
+    @Getter
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private BillingCycle billingCycle;
+
+    @Column(name="amount")
+    @Getter
+    @Setter
+    private int amount;
+
+    @Column(name="advance")
+    @Getter
+    @Setter
+    private int advance;
+
+}
