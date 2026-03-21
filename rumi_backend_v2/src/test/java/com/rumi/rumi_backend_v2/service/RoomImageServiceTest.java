2d136b0b0f9c2289ef057748b827568b15be296f
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-21 09:26:56 +0530

    add two file for unit testing for room image upload and fetch logic

---
+++ rumi_backend_v2/src/test/java/com/rumi/rumi_backend_v2/service/RoomImageServiceTest.java
@@ -0,0 +1,16 @@
+package com.rumi.rumi_backend_v2.service;
+
+import org.junit.jupiter.api.Test;
+
+public class RoomImageServiceTest {
+
+    @Test  //This a test annotation for this upload room image method
+    void testUploadRoomImage(){
+        //System.out.println("Test run");
+    }
+
+    @Test
+    void testFetchRoomImage(){
+
+    }
+}
