343c2d2fc4c955c2c30a1519daee14ea58182117
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2026-03-22 21:54:59 +0530

    fixing react-dom bug

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,13 +3,6 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-jest.mock("react-router-dom", () => ({
-  BrowserRouter: ({ children }) => children,
-  Routes: ({ children }) => children,
-  Route: () => null,
-  Navigate: () => null,
-}));
-
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
