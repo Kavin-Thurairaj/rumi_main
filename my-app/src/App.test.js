9068acd44aea03ee581ad1a2388c1b184bc54eca
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2026-03-22 21:51:10 +0530

    fixing test error

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
 
-test('renders learn react link', () => {
+jest.mock("react-router-dom", () => ({
+  BrowserRouter: ({ children }) => children,
+  Routes: ({ children }) => children,
+  Route: () => null,
+  Navigate: () => null,
+}));
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
